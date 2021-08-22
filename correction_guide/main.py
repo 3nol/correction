08acf5bc48bc26f9b231f468231e8d0dfd4dee77
Leon Wenzler <leon.wenzler@gmail.com>
2021-08-22 13:46:37 +0200

    adding database points storage

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -3,5 +3,6 @@ from Paths import source_path
 
 if __name__ == '__main__':
 
+    # init_tutti_names(['clara_biedermann', 'jannes_mueller', 'leon_wenzler'])
     # Solution.generate_all_solution_templates('01')
-    Correction(source_path, '01', 'Clara').start()
+    Correction(source_path, '01', 'Leon Wenzler').start()
