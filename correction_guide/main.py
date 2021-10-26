15d1a3ce3f2c5a56bac25329d91dac281e8081bd
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2021-10-26 11:10:07 +0200

    commenting Correction.py, renaming methods

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
 
     # init_tutti_names(['clara_biedermann', 'jannes_mueller', 'leon_wenzler'])
     # Solution.generate_all_solution_templates('01')
-    Correction(source_path, '01', 'Clara Biedermann').start()
+    Correction(source_path, '01', 'Clara Biedermann').setup()
