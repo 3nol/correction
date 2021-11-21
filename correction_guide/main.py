3a675a5fc12748e2cc9bfc4422435edf34467be6
Clara Biedermann <clara.biedermann@uni-konstanz.de>
2021-11-21 13:37:00 +0100

    changing accidental committed changes back

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -1,9 +1,7 @@
 from Correction import Correction, init_tutti_names
 from Paths import source_path
-from Utility import insert_total_in_db
 
 
 if __name__ == '__main__':
     # init_tutti_names(['clara.biedermann', 'jannes.mueller', 'leon.wenzler'])
-    # Correction(source_path, '03').setup()
-    insert_total_in_db('02')
+    Correction(source_path, '01').setup()
