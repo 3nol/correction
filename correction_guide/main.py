5fc223e144dcb2d544ba59e1789bfa42d8bd464c
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2021-11-22 10:31:00 +0100

    enabling cli arguments

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -1,6 +1,7 @@
+import sys
 from Correction import Correction
 from Paths import source_path
 
 
 if __name__ == '__main__':
-    Correction(source_path, '03').setup()
+    Correction(source_path, sys.argv[1].zfill(2)).setup()
