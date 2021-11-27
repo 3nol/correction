fed9272c7682455acb6084dc502a1d1198644292
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2021-11-27 14:58:05 +0100

    updating file_ignore.txt

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -4,4 +4,5 @@ from Paths import source_path
 
 
 if __name__ == '__main__':
-    Correction(source_path, sys.argv[1].zfill(2)).setup()
+    Correction(source_path, '04').setup()
+    # Correction(source_path, sys.argv[1].zfill(2)).setup()
