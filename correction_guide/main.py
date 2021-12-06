a08a36a31101eb0da7bf84b7f1f4f8a0ed089673
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2021-12-06 18:29:51 +0100

    shutting down correction script gracefully

--- correction_guide/main.py
+++ correction_guide/main.py
@@ -5,15 +5,20 @@ from Correction import Correction
 from Paths import source_path
 
 if __name__ == '__main__':
-    ass_number = str(sys.argv[1]).rsplit('0', 1)[1]
-    if len(sys.argv) > 2 and sys.argv[2] in ['-s', '--solution-only']:
-        tutti_names = []
-        for student in [f.path for f in os.scandir(source_path) if f.is_dir() and '***REMOVED***' not in f.path]:
-            for directory, _, files in os.walk(student):
-                if 'git' not in directory and 'concatenated' not in directory and 'feedback' not in directory:
-                    if any(list(map(lambda f: ass_number in directory.split(student)[1] or ass_number in f, files))):
-                        if student not in tutti_names:
-                            tutti_names.append(student)
-        Correction(source_path, ass_number.zfill(2), tutti_names=list(set(tutti_names))).setup()
-    else:
-        Correction(source_path, ass_number.zfill(2)).setup()
+    try:
+        ass_number = str(sys.argv[1]).rsplit('0', 1)[1]
+        if len(sys.argv) > 2 and sys.argv[2] in ['-s', '--solution-only']:
+            tutti_names = []
+            for student in [f.path for f in os.scandir(source_path) if f.is_dir() and '***REMOVED***' not in f.path]:
+                for directory, _, files in os.walk(student):
+                    if 'git' not in directory and 'concatenated' not in directory and 'feedback' not in directory:
+                        if any(list(map(lambda f: ass_number in directory.split(student)[1] or ass_number in f, files))):
+                            if student not in tutti_names:
+                                tutti_names.append(student)
+            Correction(source_path, ass_number.zfill(2), tutti_names=list(set(tutti_names))).setup()
+        else:
+            Correction(source_path, ass_number.zfill(2)).setup()
+        exit(0)
+    except KeyboardInterrupt:
+        print('\nVery understandable, have a nice day! :)')
+        exit(1)
