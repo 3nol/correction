e23e5679c0992f57782e1f403d2abff8f9c33bac
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-01-17 12:35:43 +0100

    removes printing black lines

--- correction_guide/Paths.py
+++ correction_guide/Paths.py
@@ -1,5 +1,5 @@
 import os
 
 config_path = f'{os.getcwd().rsplit(os.path.sep, 1)[0]}{os.path.sep}'
-source_path = f'{os.getcwd().rsplit(os.path.sep, 1)[0]}{os.path.sep}example_source{os.path.sep}'
-corrector = 'Clara Biedermann'
+source_path = f'{os.getcwd().rsplit(os.path.sep, 2)[0]}{os.path.sep}students{os.path.sep}'
+corrector = 'Leon Wenzler'
