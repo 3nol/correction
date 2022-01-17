feefbe4bbca9ccf6b5c694462afdc553aaf6f885
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-01-17 12:36:15 +0100

    undoing changes in Paths.py

--- correction_guide/Paths.py
+++ correction_guide/Paths.py
@@ -2,4 +2,4 @@ import os
 
 config_path = f'{os.getcwd().rsplit(os.path.sep, 1)[0]}{os.path.sep}'
 source_path = f'{os.getcwd().rsplit(os.path.sep, 2)[0]}{os.path.sep}students{os.path.sep}'
-corrector = 'Leon Wenzler'
+corrector = 'Clara Biedermann'
