f0c118e3b1f31ba9882f3543150f68ed7757da3e
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-02-25 18:43:43 +0100

    moving files, removing unused ones

--- assignment_handler/Paths.py
+++
@@ -1,5 +0,0 @@
-import os
-
-config_path = f'{os.getcwd().rsplit(os.path.sep, 1)[0]}{os.path.sep}'
-source_path = f'{os.getcwd().rsplit(os.path.sep, 2)[0]}{os.path.sep}students{os.path.sep}'
-corrector = 'Leon Wenzler'
