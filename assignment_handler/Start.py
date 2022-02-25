aafaf1926000ed2b51d3999939e3a24b99a86e00
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-02-25 18:56:55 +0100

    renaming students

--- assignment_handler/Start.py
+++ assignment_handler/Start.py
@@ -7,15 +7,15 @@ from assignment_handler.feedback_handler.Correction import Correction
 
 
 def collect_solution_only_students():
-    tutti_names = set()
+    student_names = set()
     for student in [f.path for f in os.scandir(gc.get('source_path')) if f.is_dir()]:
         for directory, _, files in os.walk(student):
             # don't search in ignored folders, then collect students who have done the assignment
             if not any([d for d in [gc.get('concat_folder'), gc.get('feedback_folder')] + gc.get('excluded_folders')
                         if d in directory]) and \
                    any(map(lambda f: ass_number in directory.split(student)[1] or ass_number in f, files)):
-                tutti_names.add(student)
-    return list(tutti_names)
+                student_names.add(student)
+    return list(student_names)
 
 
 if __name__ == '__main__':
