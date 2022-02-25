312775792acb02b5ba6e89b3ffe89627d9f3409f
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-02-25 12:25:17 +0100

    setting up the config file with GlobalConstants

---
+++ assignment_handler/config.py
@@ -0,0 +1,40 @@
+
+class GlobalConstants:
+
+    __conf = {
+        'correcting_person': 'Chuck Norris',
+        'source_path': '../assignments',
+        'points_': {
+            '01': [[3, 2], [3], [2]],
+            '02': [[10], [0]],
+            '03': [[2, 1, 2, 2], [3]],
+            '04': [[1, 1], [1, 1, 2, 2, 2]],
+            '05': [[1], [3, 3, 3]],
+            '06': [[3], [3], [2, 2]],
+            '07': [[5, 5]],
+            '08': [[1, 1, 1, 3], [0.5, 2, 0.5, 1]],
+            '09': [[1, 1, 1], [1, 1, 1, 1, 2, 1]],
+            '10': [[1, 1, 1], [1, 1, 1], [4]],
+            '11': [[1, 3, 6]]
+        },
+        'db': {
+            'user': 'root',
+            'password': 'p\'d3@\'dGwL4e;Yxa',
+            'host': 'port.halloibims.com',
+            'port': 7001,
+            'database': 'sq'
+        },
+        'concat_folder': 'concatenated',
+        'feedback_folder': 'feedback',
+        'excluded_names': ['Chuck Norris', 'Bruce Lee']
+    }
+
+    @staticmethod
+    def get(key: str) -> any:
+        try:
+            if key.startswith('points_'):
+                return GlobalConstants.__conf['points_'][key.split('points_', 1)[1].zfill(2)]
+            return GlobalConstants.__conf[key]
+        except KeyError:
+            print(f'GlobalConstant not found: {key}')
+        return None
