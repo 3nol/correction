538c5689ae111d2c0689b99b56e0b24f19eb1d82
Leon Wenzler <leon.wenzler@uni-konstanz.de>
2022-02-27 21:45:05 +0100

    fixing typo

--- assignment_handler/Start.py
+++ assignment_handler/Start.py
@@ -15,7 +15,7 @@ def collect_solution_only_students(ass_number: str) -> list[str]:
             if not any([d for d in [gc.get('concat_folder'), gc.get('feedback_folder')] + gc.get('excluded_folders')
                         if d in directory]) and \
                    any(map(lambda f: ass_number in directory.split(student)[1] or ass_number in f, files)):
-                # this students has a file or folder with the ass_number present somewhere
+                # this student has a file or folder with the assignment number present somewhere
                 student_names.add(student)
     return list(student_names)
 
@@ -26,10 +26,10 @@ def handle_cli_args(args: list) -> None:
         - 2. optional flag for solutions only: -s or --solution-only (see method above)"""
     try:
         students = None
-        # parse ass_number neatly
+        # parse assignment number neatly
         ass_number = ''.join(dropwhile(lambda c: c == '0', str(args[1])))
         if len(args) > 2 and args[2] in ['-s', '--solution-only']:
-            # solution only flag is present
+            # solution-only flag is present
             students = collect_solution_only_students(ass_number)
         # start the correction by using the source path and the collected students
         Correction(gc.get('source_path'), ass_number.zfill(2), student_names=students).setup()
@@ -40,6 +40,7 @@ def handle_cli_args(args: list) -> None:
 
 
 if __name__ == '__main__':
+    sys.argv = ['blub', '02']
     # inserts all given names into the database
     # init_names([name1, name2, name3, ...])
 
